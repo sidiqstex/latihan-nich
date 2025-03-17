8bb021339d784470b60e7a938097a4c9f88f7860
sidiqstex <sidiqzainudin47@gmail.com>
2025-03-17 12:33:54 +0700

    upate column artikel

--- database/migrations/2025_03_17_043929_create_articles_table.php
+++ database/migrations/2025_03_17_043929_create_articles_table.php
@@ -14,6 +14,9 @@ return new class extends Migration
         Schema::create('articles', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
+            $table->string('title');
+            $table->string('description');
+            $table->string('slug');
         });
     }
 

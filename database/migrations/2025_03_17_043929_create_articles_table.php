9a2f68e99011f77f52cd4bc27166637cdbc077d3
sidiqstex <sidiqzainudin47@gmail.com>
2025-03-17 12:40:48 +0700

    tambah created date

--- database/migrations/2025_03_17_043929_create_articles_table.php
+++ database/migrations/2025_03_17_043929_create_articles_table.php
@@ -17,6 +17,7 @@ return new class extends Migration
             $table->string('title');
             $table->string('description');
             $table->string('slug');
+            $table->timestamp('created_at')->nullable();
         });
     }
 

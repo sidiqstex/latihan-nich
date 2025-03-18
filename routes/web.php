d6302463a54384db9eb9f6d0000e83bf8bcfdd05
sidiqstex <sidiqzainudin47@gmail.com>
2025-03-18 13:57:52 +0700

    middleware perumahan

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,24 @@
 <?php
 
+use App\Http\Controllers\ArticleController;
+use App\Http\Controllers\PerumahanController;
+use App\Http\Middleware\EnsureIsUserPerumahan;
+use App\Http\Middleware\IzinTinggalPerumahan;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::middleware([EnsureIsUserPerumahan::class, IzinTinggalPerumahan::class])->group(function () {
+
+    Route::get('/article', [ArticleController::class, 'index']);
+
+    Route::get('/article/{id}/{category}', function (Request $request, $id, $category) {
+        return $id . $category . $request->order_by;
+    });
+    Route::withoutMiddleware([EnsureIsUserPerumahan::class])->group(function () {
+        Route::get('perumahan', [PerumahanController::class, 'index']);
+    });
 });
